738075ea4931b83663ed5eb1a44f367a22c8be02
Student_GA126 <student_ga126@psxga126.myfiosgateway.com>
2020-07-01 10:41:13 -0400

    Minor edits to comments

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
-// Player guesses a direction up, down, left, or right
-// CPU chooses up, down, left, or right randomly
-// Compare results
-    // If results are == to each other
-    // The player gets 1 point, and gets to guess again immediately
-    // If results are !== to each other
-    // The player loses his opportunity and the computer gets to guess a direction
-
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-const dir = ["up", "down", "left", "right"];
+//Pseudo:
 
-// const user = prompt("What's your guess?", "Up, down, left, or right?");
-// const user = "down";
-// const cpu = dir[Math.floor(Math.random() * 4)]; 
+// Ask the player which direction they want to point
+    // if the cpu looks in that direction
+    // generate random choice for 
+
+const dir = ["up", "down", "left", "right"];
 
 let userScore = 0;
 let cpuScore = 0;
 let winScore = 3; // easy difficulty.
-// console.log(cpu)
-
 
 while (userScore < winScore && cpuScore < winScore) {
     let user = prompt("What's your guess?", "Up, down, left, or right?"); // > > > I originally initialized "user" with const. But it gave me issues when I wanted to re < < <
-    const cpu = dir[Math.floor(Math.random() * 4)]; // generates random number 1 - 4 (not including 4). Randomly selects a value (a direction) from the dir (directions) array.
-    // console.log(`You point ${user}!`);
-    // console.log(`The cpu looks ${cpu}`);
+    const cpu = dir[Math.floor(Math.random() * 3)]; // generates random number 0 - 3 (not including 3, I only need 0 - 3 bc the indexes of arrays always begin at 0). Randomly selects a value from the dir (directions) array.
+
     alert (`You point ${user}!`); // "You point (player choice)"
     alert (`The cpu looks ${cpu}`); // "The cpu looks (random selection)"
 
     if (user.toLowerCase() == cpu) { // I use .toLowerCase() to account for any capitalized letters which I think would cause some issues (like "Up" may not be == "up", although full disclosure, I'm not 100% sure on that.)
+
         userScore++; // If the user guesses correctly, they get 1 point added to their score.
         alert (`Nice, you get another turn! Points: ${userScore}`); // This line is the user can keep track of their score.
     }
 
     else if (user.toLowerCase() !== cpu) {
         alert (`It's the cpu's turn now!`)
-        user = prompt("Turn your head!", "Up, down, left, or right?"); // 
+        user = prompt("Turn your head!", "Up, down, left, or right?"); // Now asks which direction you should turn your head
         alert (`The cpu points ${cpu}`);
         if (cpu == user.toLowerCase()) {
-            cpuScore++; // increases cpu's score
+            cpuScore++;
             alert (`The cpu got a point. His score is: ${cpuScore}`) // This line is the user can keep track of the cpu's score.
         }
         else (alert (`The cpu guessed wrong! It's your turn now!`))
